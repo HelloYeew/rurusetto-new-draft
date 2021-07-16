9e4d2c24f1cb94b9a4e7105f58a10ea70321e5cd
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-17 04:45:11 +0700

    Add header menu

--- custom.js
+++ custom.js
@@ -5,9 +5,15 @@ function scrollFunction() {
         document.getElementById("profile-picture").style.height = "32px";
         document.getElementById("header-logo").style.height = "50px";
         document.getElementById("header").style.backgroundColor = "rgba(74,74,74,60)"
+        document.getElementById("nav-listing").style.fontSize = "18px"
+        document.getElementById("nav-install").style.fontSize = "18px"
+        document.getElementById("nav-status").style.fontSize = "18px"
     } else {
         document.getElementById("profile-picture").style.height = "48px";
         document.getElementById("header-logo").style.height = "65px";
         document.getElementById("header").style.backgroundColor = "rgba(74,74,74,0)"
+        document.getElementById("nav-listing").style.fontSize = "20px"
+        document.getElementById("nav-install").style.fontSize = "20px"
+        document.getElementById("nav-status").style.fontSize = "20px"
     }
 }
