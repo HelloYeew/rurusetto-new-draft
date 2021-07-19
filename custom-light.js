92b8a895e714f20450a8eabe754b67b43e75885c
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-20 01:38:14 +0700

    Add blur effect for header

--- custom-light.js
+++ custom-light.js
@@ -1,12 +1,10 @@
-const header = document.getElementById ('header');
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("profile-picture").style.height = "32px";
         document.getElementById("header-logo").style.height = "50px";
-        document.getElementById("header").style.backgroundColor = "rgba(74,74,74,.8)"
+        document.getElementById("header").style.backgroundColor = "rgba(255,255,255,.6)"
         document.getElementById("header").classList.add('blur-bg')
         document.getElementById("nav-listing").style.fontSize = "18px"
         document.getElementById("nav-install").style.fontSize = "18px"
@@ -14,7 +12,7 @@ function scrollFunction() {
     } else {
         document.getElementById("profile-picture").style.height = "48px";
         document.getElementById("header-logo").style.height = "65px";
-        document.getElementById("header").style.backgroundColor = "rgba(74,74,74,0)"
+        document.getElementById("header").style.backgroundColor = "rgba(255,255,255,0)"
         document.getElementById("header").classList.remove('blur-bg')
         document.getElementById("nav-listing").style.fontSize = "20px"
         document.getElementById("nav-install").style.fontSize = "20px"
