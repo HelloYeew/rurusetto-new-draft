c1a0912a792230c48a627b64c81dc3d8e0d7b734
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-22 17:19:25 +0700

    Add initial design of mobile navbar

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 const header = document.getElementById ('header');
 
 window.onscroll = function() {scrollFunction()};
+window.onresize = function() {onResize()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -21,3 +22,15 @@ function scrollFunction() {
         document.getElementById("nav-status").style.fontSize = "20px"
     }
 }
+
+function onResize() {
+    if (document.documentElement.clientWidth > 992) {
+        document.getElementById("header").classList.add('show')
+        document.getElementById("header").classList.remove('hidden')
+    } else {
+        document.getElementById("header").classList.add('hidden')
+        document.getElementById("header").classList.remove('show')
+    }
+}
+
+window.addEventListener("load", function(){onResize()})
